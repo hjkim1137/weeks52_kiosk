f4cb24ade9771091744acb9c22ecb7ccd26c4c39
김현정(Hyunjung Kim) <withcusco@gmail.com>
2023-10-30 10:56:26 +0900

    [feat] kiosk 탭 기능 완료

--- js/order.js
+++ js/order.js
@@ -3,7 +3,6 @@ let minus = document.querySelector('.minusBtn');
 let number = document.querySelector('.number');
 let reset = document.querySelector('.resetBtn');
 let orderBtn = document.querySelector('.orderBtn');
-let menu = document.querySelector('.menu');
 let initialPrice = document.getElementById('price');
 
 // fetch menu
@@ -68,9 +67,30 @@ function calculate() {
   initialPrice.textContent = totalPrice;
 }
 
+// 탭 전환 기능
+const tabs = document.querySelectorAll('.tab');
+const containers = document.querySelectorAll('.group');
+
+tabs.forEach((tab) => {
+  tab.addEventListener('click', () => {
+    // getAttribute
+    const tabNumber = tab.getAttribute('data-tab'); // 1,2,3,4
+
+    // 1. 특정 탭 클릭 이후 모든 탭의 class(selected,show) 제거
+    tabs.forEach((t) => t.classList.remove('selected'));
+    containers.forEach((c) => c.classList.remove('show'));
+
+    // 2. 선택한 특정 탭만 class명 추가(selected, show) => 즉 attribute 다시 지웠다 추가하는 것
+    tab.classList.add('selected');
+    // 예) attribute명이 data-content =1 인 요소의 클래스에 'show'추가
+    document
+      .querySelector(`[data-content="${tabNumber}"]`)
+      .classList.add('show');
+  });
+});
+
 // eventListener
 plus.addEventListener('click', plusNum);
 minus.addEventListener('click', minusNum);
 reset.addEventListener('click', resetNum);
 orderBtn.addEventListener('click', order);
-menu.addEventListener('click', addtoList);
